616dfe4174c7c2b34da2881e2501510ee2b519b1
Yuri Baldi <yuri.baldi@unifesp.br>
2022-09-26 14:36:11 -0300

    Enrollments table restrictions

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,4 +1,7 @@
 class Enrollment < ApplicationRecord
-    belongs_to :institution
+    belongs_to :institution 
     belongs_to :student
+    validates_presence_of :course_name, :institution_id, :student_id, :total_course, :number_payments, :due_day
+    validates :number_payments, numericality: { in: 1..120 }
+    validates :due_day, numericality: { in: 1..65 }
 end
