54a20547c046ed499a98b3460cc6936c4a3e0db5
Yuri Baldi <yuri.baldi@unifesp.br>
2022-09-29 15:27:18 -0300

    billinho

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  get 'home/index'
   resources :payments
   resources :enrollments
   resources :students
   resources :institutions
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+ 
+   root "home#index"
 end
