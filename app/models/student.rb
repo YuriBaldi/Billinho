2df4aacd8776006f96ead3f871137cd600c444dd
hevslc <hevelyn.sthefany@gmail.com>
2022-10-05 13:10:32 -0300

    add: student restrictions

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,7 @@
 class Student < ApplicationRecord
     has_many :enrollments
+    validates_presence_of :name, :cpf, :gender, :payment_type
+    validates :cpf, numericality: {only_integer: true, message: "CPF must be a number"}
+    validates :gender, inclusion: {in: %w{"m", "f"}, message: "Student gender must be 'f' (female) or 'm' (male)"}
+    validates :payment_type, inclusion: {in: %w{"m", "f"}, message: "Student gender must be 'boleto' or 'card'"}
 end
