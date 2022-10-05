60ef0af8bd553c855fccdd1180d8e0b9252f785b
hevslc <hevelyn.sthefany@gmail.com>
2022-10-05 12:39:49 -0300

    fix: due_day, institution_type

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -3,5 +3,5 @@ class Enrollment < ApplicationRecord
     belongs_to :student
     validates_presence_of :course_name, :institution_id, :student_id, :total_course, :number_payments, :due_day
     validates :number_payments, numericality: { in: 1..120 }
-    validates :due_day, numericality: { in: 1..65 }
+    validates :due_day, numericality: { in: 1..31 }
 end
