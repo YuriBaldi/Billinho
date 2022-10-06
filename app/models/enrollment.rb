a418cabeded7e6c76f4a40a9f31825b0ab9cce96
Yuri Baldi <yuri.baldi@unifesp.br>
2022-10-06 17:00:47 -0300

    add restrictions to student removal

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,6 +1,7 @@
 class Enrollment < ApplicationRecord
     belongs_to :institution 
     belongs_to :student
+    has_one :payment, dependent :destroy
     validates_presence_of :course_name, :institution_id, :student_id, :total_course, :number_payments, :due_day
     validates :number_payments, numericality: { in: 1..120 }
     validates :due_day, numericality: { in: 1..65 }
