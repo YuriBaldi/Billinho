8c7791130ffd3a13bde7e0f86e6ae661e54f5423
Yuri Baldi <yuri.baldi@unifesp.br>
2022-10-06 15:58:15 -0300

    add foreign key enrollment_id to payments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_27_164342) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_06_180048) do
   create_table "enrollments", force: :cascade do |t|
     t.decimal "course_price"
     t.integer "number_payments"
@@ -32,6 +32,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_27_164342) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "islands", force: :cascade do |t|
+    t.string "name"
+    t.string "arc"
+    t.string "type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "payments", force: :cascade do |t|
     t.decimal "value"
@@ -39,6 +46,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_27_164342) do
     t.string "status", default: "open"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "enrollments_id", null: false
+    t.index ["enrollments_id"], name: "index_payments_on_enrollments_id"
   end
 
   create_table "students", force: :cascade do |t|
@@ -52,4 +61,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_27_164342) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "payments", "enrollments", column: "enrollments_id"
 end
